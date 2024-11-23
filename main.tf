f3b3e4804558a52a6c9cf8640a0d66ea327e1093
Rajesh-gandham-143 <gandamrajesh143@gmail.com>
2024-11-23 12:39:35 +0000

    changes made

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance1" {
     ami = "ami-083644ad45483a44f"
-    isntance_type = "t2.micro"
+    instance_type = "t2.micro"
     tags = {
         name = "server1"
     }
